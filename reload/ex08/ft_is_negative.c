8fcb099c33bc3da58216f4e76e29da04e9854a6c
Don <neveradio@gmail.com>
2024-02-20 14:52:42 +0900

    ex12

---
+++ reload/ex08/ft_is_negative.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void    ft_is_negative(int n)
+{
+    if (n < 0)
+    write(1, "N", 1);
+    else
+    write(1, "P", 1);
+}
+/*
+#include <stdlib.h>
+int main(int ac, char **av)
+{
+    (void)ac;
+    ft_is_negative(atoi(av[1]));
+    return (0);
+}
+*/
