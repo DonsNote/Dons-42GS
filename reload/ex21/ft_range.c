28562980d767cd4142b02134c5f4146f3cbafc71
Don <neveradio@gmail.com>
2024-02-22 16:57:17 +0900

    tabtest

--- reload/ex21/ft_range.c
+++ reload/ex21/ft_range.c
@@ -23,6 +23,8 @@ int	*ft_range(int min, int max)
 	sol = (int *)malloc(sizeof(int) * (max - min));
 	if (nbr == 0)
 		return (0);
+	if (min >= max)
+		return (0);
 	while (i < (max - min))
 	{
 		sol[i] = nbr;
