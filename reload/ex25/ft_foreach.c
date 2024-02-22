8706f1ed997ed99b39c71f89f54f4476c087f49e
Don <neveradio@gmail.com>
2024-02-22 09:27:14 +0900

    ex25-t

--- reload/ex25/ft_foreach.c
+++ reload/ex25/ft_foreach.c
@@ -1,5 +1,18 @@
 
+#include <unistd.h>
+
+void    ft_putnbr(int nbr)
+{
+    char    c;
+
+    c = nbr + 48;
+    write(1, &c, 1);
+}
+
+void    (*f)(int);
+f = ft_putnbr;
+
 void    ft_foreach(int *tab, int lengthm void (*f)(int))
 {
-    
+
 }
