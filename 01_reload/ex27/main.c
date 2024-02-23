79e3b87ea69980a00f9048211d6f480b957572d7
Dohyun Kim <dohyuki2@c1r2s4.42gyeongsan.kr>
2024-02-23 10:29:00 +0900

    fix_ex27

--- 01_reload/ex27/main.c
+++ 01_reload/ex27/main.c
@@ -1,40 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 09:46:31 by dohyuki2          #+#    #+#             */
+/*   Updated: 2024/02/23 10:27:27 by dohyuki2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 
-void    errmsg(int i)
+void	errmsg(int i)
 {
-    if (i == 1)
-        write (2, "File name missing.\n", 19);
-    if (i == 2)
-        write (2, "Too many arguments.\n", 20);
-    if (i == 3)
-        write(2, "Cannot read file.\n", 18);
+	if (i == 1)
+		write (2, "File name missing.\n", 19);
+	if (i == 2)
+		write (2, "Too many arguments.\n", 20);
+	if (i == 3)
+		write(2, "Cannot read file.\n", 18);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    int     i;
-    int     file;
-    char    buff[1024];
+	int		i;
+	int		file;
+	char	buff[1024];
 
-    i = 0;
-    if (ac <= 1)
-        errmsg(1);
-    else if (ac > 2)
-        errmsg(2);
-    else if (ac == 2)
-    {
-        if ((file = open(av[1], O_RDWR)) > 0)
-        {
-            read (file, buff, 1024);
-            while (buff[i] != '\0')
-                write (1, &buff[i++], 1);
-            close (file);
-        }
-        else
-            errmsg(3);
-    }
-    return (0);
+	i = 0;
+	file = open(av[1], O_RDWR);
+	if (ac <= 1)
+		errmsg(1);
+	else if (ac > 2)
+		errmsg(2);
+	else if (ac == 2)
+	{
+		if (file > 0)
+		{
+			read (file, buff, 1024);
+			while (buff[i] != '\0')
+				write (1, &buff[i++], 1);
+			close (file);
+		}
+		else
+			errmsg(3);
+	}
+	return (0);
 }
 /*
     fd = open(av[1], O_RDWR);
