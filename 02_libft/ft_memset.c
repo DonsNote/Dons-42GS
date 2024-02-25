a3fc3e897bc7756521d27e7e551f6c85a49286b8
Don <neveradio@gmail.com>
2024-02-26 08:44:15 +0900

    fix_gitig

--- 02_libft/ft_memset.c
+++ 02_libft/ft_memset.c
@@ -1,7 +1,28 @@
 
-#include "libc.h"
+#include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((unsigned char*)b)[i] = (unsigned char)c;
+		++i;
+	}
+	return (b);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char a[] = "dddddddddd";
+	char *ft_sol = ft_memset(a, 65, 10);
+	char *sol = memset(a, 65, 10);
+	printf("%s\n", ft_sol);
+	printf("%s\n", sol);
+	return (0);
 }
