9d5493e4de6246378abbc76c40675cae33b9697c
Don <neveradio@gmail.com>
2024-03-07 03:11:33 +0900

    fix_strtrim

--- 02_libft/ft_strtrim.c
+++ 02_libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: don <don@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 11:04:11 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/03/04 12:25:36 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/03/07 02:55:15 by don              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,51 +26,39 @@ static	size_t	check_set(char c, char const *set)
 	return (0);
 }
 
-static	char	*ft_dupcpy(char const *s, size_t size, size_t i)
-{
-	size_t	j;
-	size_t	k;
-	char	*sol;
-
-	j = i - size;
-	k = 0;
-	sol = (char *)malloc(sizeof(char) * (size + 1));
-	if (sol == 0)
-		return (0);
-	while (size > 0)
-	{
-		sol[k] = s[j];
-		++k;
-		++j;
-		--size;
-	}
-	sol[k] = '\0';
-	return (sol);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	size;
+	size_t	j;
 	char	*sol;
 
 	i = 0;
-	size = 0;
-	if (s1[i] == 0)
-		return (0);
-	if (set[i] == 0)
+	j = ft_strlen(s1) - 1;
+	if (!s1)
+		return ((char *)0);
+	if (!set)
 		return (ft_strdup(s1));
-	while (check_set((char)s1[i], set))
+	while (s1[i] != '\0' && check_set((char)s1[i], set))
 		++i;
-	while (s1[i] != '\0')
-	{
-		if (check_set((char)s1[i], set))
-			break ;
-		++size;
-		++i;
-	}
-	sol = ft_dupcpy(s1, size, i);
+	while (j >= i && check_set((char)s1[j], set))
+		--j;
+	sol = ft_substr(s1, i, (j - i + 1));
 	if (sol == 0)
 		return (0);
 	return (sol);
 }
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char s[] = "lorem ipsum dolor sit amet";
+	char set[] = "l ";
+	char *sol = ft_strtrim(s, set);
+
+	printf("%s\n", sol);
+	free(sol);
+	return (0);
+}
+*/
