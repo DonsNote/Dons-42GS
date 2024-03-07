75251c162bbb387ee5e8fb5187b5103d66567b09
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-03-07 13:26:18 +0900

    move_d_on_cle

---
+++ 02_libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 12:57:16 by dohyuki2          #+#    #+#             */
+/*   Updated: 2024/03/07 12:59:22 by dohyuki2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list) * 1);
+	if (new)
+	new->content = content;
+	new->next = 0;
+	return (new);
+}
