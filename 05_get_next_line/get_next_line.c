88c809c2b12f25150e00a0ed7d6344018c838896
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-04-02 09:46:16 +0900

    gnlstart

--- 05_get_next_line/get_next_line.c
+++ 05_get_next_line/get_next_line.c
@@ -3,10 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: don <don@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 17:58:02 by don               #+#    #+#             */
-/*   Updated: 2024/03/31 17:59:29 by don              ###   ########.fr       */
+/*   Updated: 2024/04/01 20:28:58 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	ssize_t	file;
+	void	*buf; // 파일을 읽어 들일 버퍼
+	size_t	BUFFER_SIZE; // 버퍼의 크기 컴파일 시 -D BUFFER_SIZE=n 로 define 함
+
+
+	file = read(fd, buf, BUFFER_SIZE);
+	if (file == -1)
+		return (-1);
+	
+
+}
--- 05_get_next_line/get_next_line.h
+++ 05_get_next_line/get_next_line.h
@@ -3,14 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: don <don@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 17:58:55 by don               #+#    #+#             */
-/*   Updated: 2024/03/31 17:59:27 by don              ###   ########.fr       */
+/*   Updated: 2024/04/01 20:28:56 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char	*get_next_line(int fd);
 
 #endif
