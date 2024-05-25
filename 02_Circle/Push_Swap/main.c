0b571b54c5611d85a1ab0314802593f5f799f7be
DonsNote <neveradio@gmail.com>
2024-05-25 18:35:51 +0900

    makestack_done_startfunc

--- 02_Circle/Push_Swap/main.c
+++ 02_Circle/Push_Swap/main.c
@@ -6,11 +6,12 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 09:10:32 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/05/22 17:38:04 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/05/25 18:29:31 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
@@ -27,6 +28,19 @@ int	main(int ac, char **av)
 	i = make_num(ac, av, &size);
 	a = make_stack(i, size);
 	free(i);
+
+	t_list	*tmp;
+	int j;
+
+	j = 0;
+	tmp = a;
+	while (j < size)
+	{
+		printf("size : %d\n index : %d\n", size, j);
+		printf("sol : %d\n", tmp->num);
+		tmp = tmp->next;
+		++j;
+	}
 //	sol = push_swap(a);
 	return (0);
 }
