e8e4a97954925c79625490730d02cb858ec071e3
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-07-24 16:54:49 +0900

    gotopt

--- 02_Circle/Push_Swap/push_swap.c
+++ 02_Circle/Push_Swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 08:19:49 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/24 14:59:47 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/24 16:53:37 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	push_swap(t_list *a, t_list *b, int size)
 {
-	push_a(a, b);
+	a = rotate_a(a);
 	for(t_list *tmp = a; tmp != 0; tmp = tmp->next)
 		printf("num(A) : %d\n", tmp->num);
+	b = push_a(a, b);
 	for(t_list *tmpb = b; tmpb != 0; tmpb = tmpb->next)
 		printf("num(B) : %d\n", tmpb->num);
 	return ;
