7eea92772f4bbedc2e6436c3f07bceceaaa70dcc
DonsNote <neveradio@gmail.com>
2024-07-24 22:58:08 +0900

    cha

--- 02_Circle/Push_Swap/push_swap.c
+++ 02_Circle/Push_Swap/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 08:19:49 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/24 16:53:37 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/24 22:45:42 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	push_swap(t_list *a, t_list *b, int size)
 {
-	a = rotate_a(a);
+	// b = push_a(a, b);
+	// a = rotate_a(a);
 	for(t_list *tmp = a; tmp != 0; tmp = tmp->next)
 		printf("num(A) : %d\n", tmp->num);
-	b = push_a(a, b);
 	for(t_list *tmpb = b; tmpb != 0; tmpb = tmpb->next)
 		printf("num(B) : %d\n", tmpb->num);
 	return ;
