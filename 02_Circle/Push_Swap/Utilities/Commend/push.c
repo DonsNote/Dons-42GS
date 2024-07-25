08b10dec3affb98a4f847bcf39fa469ced74a088
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-07-26 00:37:26 +0900

    tosleeping

--- 02_Circle/Push_Swap/Utilities/Commend/push.c
+++ 02_Circle/Push_Swap/Utilities/Commend/push.c
@@ -6,34 +6,49 @@
 /*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 10:36:35 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/25 22:42:49 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/26 00:33:05 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
+void	push_util(t_list **a, t_list **b)
+{
+	if ((*b)->prev == NULL)
+	{
+		(*a)->prev = *b;
+		(*b)->prev = *a;
+	}
+	else
+	{
+		(*a)->prev = (*b)->prev;
+		(*b)->prev->next = *a;
+	}
+	(*a)->next = *b;
+	(*b)->prev = *a;
+	return ;
+}
+
 void	push_a(t_list **a, t_list **b)
 {
 	t_list	*tmp;
+	t_list	*btmp;
 
+	if (*a == NULL)
+		return ;
 	tmp = (*a)->next;
-	tmp->prev = (*a)->prev;
-	tmp->prev->next = tmp;
+	btmp = *a;
+	(*a)->next->prev = (*a)->prev;
+	(*a)->prev->next = (*a)->next;
 	if (*b == NULL)
 	{
 		(*a)->next = NULL;
 		(*a)->prev = NULL;
-		*b = *a;
-		*a = tmp;
 	}
 	else
-	{
-		(*a)->next = (*b);
-		(*a)->prev = (*b)->prev;
-		(*b)->prev->next = (*a);
-		(*b)->prev = (*a);
-		*b = *a;
-	}
+		push_util(a, b);
+	*a = tmp;
+	*b = btmp;
 	write(1, "pa\n", 3);
 	return ;
 }
--- 02_Circle/Push_Swap/push_swap.c
+++ 02_Circle/Push_Swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 08:19:49 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/25 22:40:30 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/26 00:32:35 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,7 @@ void	push_swap(t_list *a, t_list *b, int size)
 	push_a(&a, &b);
 	push_a(&a, &b);
 
-	printf("Num(A) : %d\n", a->num);
-	printf("Num(A) : %d\n", a->next->num);
-	// for(int i = 0; i < size; ++i)
-	// {
-	// 	if (b == NULL)
-	// 		break ;
 	printf("Num(B) : %d\n", b->num);
-	printf("Num(B) : %d\n", b->next->num);
-	// 	b = b->next;
-	// }
+	// printf("Num(B) : %d\n", b->next->num);
 	return ;
 }
