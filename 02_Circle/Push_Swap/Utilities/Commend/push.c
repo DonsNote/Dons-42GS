e53d19556fb0144d7a7236c0bf4cf0882f31ee71
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-07-25 22:43:03 +0900

    try

--- 02_Circle/Push_Swap/Utilities/Commend/push.c
+++ 02_Circle/Push_Swap/Utilities/Commend/push.c
@@ -3,38 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 10:36:35 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/25 17:50:15 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/25 22:42:49 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-t_list	*push_a(t_list *a, t_list **b)
+void	push_a(t_list **a, t_list **b)
 {
 	t_list	*tmp;
 
-	tmp = a->next;
-	tmp->prev = a->prev;
-	a->prev->next = tmp;
+	tmp = (*a)->next;
+	tmp->prev = (*a)->prev;
+	tmp->prev->next = tmp;
 	if (*b == NULL)
 	{
-		a->prev = NULL;
-		a->next = NULL;
-		*b = a;
+		(*a)->next = NULL;
+		(*a)->prev = NULL;
+		*b = *a;
+		*a = tmp;
 	}
 	else
 	{
-		tmp = *b;
-		a->next = tmp;
-		a->prev = tmp->prev;
-		tmp->prev->next = a;
-		tmp->prev = a;
-		*b = a;
-		a = tmp;
+		(*a)->next = (*b);
+		(*a)->prev = (*b)->prev;
+		(*b)->prev->next = (*a);
+		(*b)->prev = (*a);
+		*b = *a;
 	}
 	write(1, "pa\n", 3);
-	return (tmp);
+	return ;
 }
