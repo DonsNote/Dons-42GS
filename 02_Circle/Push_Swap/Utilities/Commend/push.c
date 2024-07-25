809ac54b51594a1c44094e0a2ac9ae7f5ad4a317
DonsNote <neveradio@gmail.com>
2024-07-25 17:51:09 +0900

    wor

--- 02_Circle/Push_Swap/Utilities/Commend/push.c
+++ 02_Circle/Push_Swap/Utilities/Commend/push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 10:36:35 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/07/25 13:36:28 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/07/25 17:50:15 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,13 @@ t_list	*push_a(t_list *a, t_list **b)
 	}
 	else
 	{
-		a->next = *b;
-		a->prev = (*b)->prev;
-		(*b)->prev->next = a;
-		(*b)->prev = a;
+		tmp = *b;
+		a->next = tmp;
+		a->prev = tmp->prev;
+		tmp->prev->next = a;
+		tmp->prev = a;
 		*b = a;
+		a = tmp;
 	}
 	write(1, "pa\n", 3);
 	return (tmp);
