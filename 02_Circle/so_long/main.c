749262069bd4b43afb724abe777c86538acfb5ba
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-08-01 17:40:42 +0900

    mlx_start

--- 02_Circle/so_long/main.c
+++ 02_Circle/so_long/main.c
@@ -6,21 +6,24 @@
 /*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 10:18:33 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/01 11:10:41 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/08/01 16:44:07 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	// int		fd;
-	// char	*map;
+	int		fd;
+	char	*map;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-	// fd = open("./*.ber", O_RDONLY);
+	// if (ac != 3)
+	// 	return (0);
+	// fd = open(av[2], O_RDONLY);
 	// map = get_next_line(fd);
-
-	ft_printf("I`m here, %d\n", 85);
-
-	return (0);
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 800, 600, "So_long");
+	mlx_loop(mlx_ptr);
 }
