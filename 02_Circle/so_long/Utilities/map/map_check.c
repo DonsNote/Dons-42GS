0dd3c44085f73696c7b319beeebafb052953f0d4
DonsNote <neveradio@gmail.com>
2024-08-04 08:12:47 +0900

    cler

--- 02_Circle/so_long/Utilities/map/map_check.c
+++ 02_Circle/so_long/Utilities/map/map_check.c
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   map_check.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/31 10:18:33 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/04 00:49:58 by dohyuki2         ###   ########.fr       */
+/*   Created: 2024/08/04 00:54:59 by dohyuki2          #+#    #+#             */
+/*   Updated: 2024/08/04 00:55:32 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "../../so_long.h"
 
-int	main(int ac, char **av)
-{
-	t_src	*src;
-
-	if (ac != 2)
-		return (0);
-	if (param_check(av[1]))
-		return (0);
-	src = (t_src *)malloc(sizeof(t_src) * 1);
-	if (src == NULL)
-		return (0);
-	if (map_size(&src, av[1]))
-		return (0);
-	start_window(&src);
-	// free(src);
-	return (0);
-}
