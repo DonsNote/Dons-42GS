842ed23cc13e8def7d1277ca364ca86c6b661faa
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-08-07 12:48:36 +0900

    minitalk_start

--- 02_Circle/so_long/Utilities/map/make_map.c
+++ 02_Circle/so_long/Utilities/map/make_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   make_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 22:02:42 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/07 06:46:00 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/08/07 10:52:35 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	map_size(t_src **src, char *av)
 
 	map_h = 0;
 	fd = open(av, O_RDONLY);
+	if (fd < 0)
+		return (1);
 	line = get_next_line(fd);
 	(*src)->wid = ft_strlen(line) - 1;
 	while (line)
