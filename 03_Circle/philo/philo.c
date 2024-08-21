06ddafc54d465b8398406631ca906ac8ea02440a
Don <neveradio@gmail.com>
2024-08-21 11:06:07 +0900

    nor

--- 03_Circle/philo/philo.c
+++ 03_Circle/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:26:10 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/20 14:39:33 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/08/21 11:05:47 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int ac, char **av)
 {
 	pthread_t	aristo;
 
+	if (ac != 5 || ac != 6)
+		return (-1);
 	pthread_create(aristo, NULL, make_fork(), NULL);
 	return (0);
 }
