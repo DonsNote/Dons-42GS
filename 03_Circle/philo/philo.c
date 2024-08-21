a02d5245d7214920e75bdf95593d4a1708b97af5
Dohyun Kim <dohyuki2@c3r5s2.42gyeongsan.kr>
2024-08-21 16:48:23 +0900

    fix_little

--- 03_Circle/philo/philo.c
+++ 03_Circle/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:26:10 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/21 16:34:44 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/08/21 16:46:33 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*start_thd(void *time)
 
 	tmp = (t_time *)time;
 	printf("philo : %d\n", tmp->number_of_philosophers);
-	return ;
+	return ((void *)tmp);
 }
 
 t_time	*time_init(char **av)
@@ -42,10 +42,10 @@ int	main(int ac, char **av)
 	pthread_t	aristo;
 	t_time		*time;
 
-	if (ac != 5 || ac != 6)
+	if (ac != 5 && ac != 6)
 		return (-1);
 	time = time_init(av);
-	pthread_create(aristo, NULL, start_thd, (void *)time);
-	pthread_join(aristo, NULL);
+	pthread_create(&aristo, NULL, start_thd, (void *)time);
+	pthread_join(aristo, (t_time **)&time);
 	return (0);
 }
