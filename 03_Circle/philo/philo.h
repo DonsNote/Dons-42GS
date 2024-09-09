e4156c5c55199d3085c16be00f84f4968a37d51e
Don <neveradio@gmail.com>
2024-09-09 13:41:59 +0900

    phil

--- 03_Circle/philo/philo.h
+++ 03_Circle/philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:26:46 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/08/27 16:31:21 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/09/01 06:19:37 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,6 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-typedef struct s_data
-{
-	int	id;
-	int	fork;
-	t_time	*time;
-}	t_data;
-
 typedef struct s_time
 {
 	int	number_of_philosophers;
@@ -32,6 +25,13 @@ typedef struct s_time
 	int	time_to_sleep;
 	int	number_of_times_each_philosopher_must_eat;
 }	t_time;
+
+typedef struct s_data
+{
+	int		id;
+	int		fork;
+	t_time	*time;
+}	t_data;
 
 int	ft_atoi(const char *str);
 
