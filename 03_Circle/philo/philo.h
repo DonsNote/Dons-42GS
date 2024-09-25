39ac58b457c461da358623256ddcefac1e8aafbf
Don <neveradio@gmail.com>
2024-09-25 13:28:31 +0900

    home

--- 03_Circle/philo/philo.h
+++ 03_Circle/philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:26:46 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/09/24 18:20:14 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:21:29 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ typedef struct s_data
 	t_fork			*fork;
 	t_time			*time;
 }	t_data;
-
 
 int	ft_atoi(const char *str);
 int	check_error(int i);
