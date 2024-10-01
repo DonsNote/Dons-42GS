da0b624d98d9ce9ccd2a7545d22fb3638159fb77
Dohyun Kim <dohyuki2@c2r2s1.42gyeongsan.kr>
2024-10-01 16:49:55 +0900

    dead

--- 03_Circle/philo/Utilities/func/dead.c
+++ 03_Circle/philo/Utilities/func/dead.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dead.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
+/*   By: dohyuki2 <dohyuki2@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 09:08:50 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/10/01 15:27:22 by dohyuki2         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:39:30 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,19 @@ int	dead(t_data *data)
 
 int	dead_check(t_data *data)
 {
+	long	now;
 	pthread_mutex_lock(&data->death->mutex);
 	if (data->death->check == 1)
 	{
 		pthread_mutex_unlock(&data->death->mutex);
+		return (1);
+	}
+	if ((get_time(now) - data->time_eat) > data->info->time_to_die || 
+		(get_time(now) - data->time_sleep) > data->info->time_to_die ||
+		(get_time(now) - data->time_think) > data->info->time_to_die)
+	{
+		pthread_mutex_unlock(&data->death->mutex);
+		dead(data);
 		return (1);
 	}
 	pthread_mutex_unlock(&data->death->mutex);
