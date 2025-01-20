b4a8e87754420f6c4283ef5870c12a79b5dda9ab
Don <neveradio@gmail.com>
2025-01-20 22:19:05 +0900

    start_cub3d

---
+++ 04_Circle/Cub3D/cub3D.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 15:19:10 by dohyuki2          #+#    #+#             */
+/*   Updated: 2025/01/20 22:18:41 by dohyuki2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <fcntl.h>
+# include <string.h>
+
+
+#endif
