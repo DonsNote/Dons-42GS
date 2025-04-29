366402d03c1ddd2c79b56ce6db9a9b2a2e0b93ef
DonsNote <neveradio@gmail.com>
2025-04-29 10:52:32 +0900

    cpp

--- 04_Circle/Cpp_Module/ex01/PhoneBook.cpp
+++ 04_Circle/Cpp_Module/ex01/PhoneBook.cpp
@@ -1,41 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/31 16:00:09 by dohyuki2          #+#    #+#             */
-/*   Updated: 2025/04/29 10:09:54 by dohyuki2         ###   ########.fr       */
+/*   Created: 2024/12/31 16:20:38 by dohyuki2          #+#    #+#             */
+/*   Updated: 2025/04/29 10:51:31 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "PhoneBook.hpp"
 
-int	main(int ac, char **av)
+PhoneBook::Contact()
 {
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-
-	int	i;
-	int	j;
-
-	i = 1;
-	while (av[i] != NULL)
-	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			if (av[i][j] >= 'a' && av[i][j] <= 'z')
-				std::cout << (char)(av[i][j] - 32);
-			else
-				std::cout << av[i][j];
-			++j;
-		}
-		++i;
-	}
-	std::cout << std::endl;
+	first = "";
 }
+
--- 04_Circle/Cpp_Module/ex01/PhoneBook.hpp
+++ 04_Circle/Cpp_Module/ex01/PhoneBook.hpp
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   digit.cpp                                          :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dohyuki2 <dohyuki2@student.42Gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/31 16:20:38 by dohyuki2          #+#    #+#             */
-/*   Updated: 2024/12/31 16:20:39 by dohyuki2         ###   ########.fr       */
+/*   Created: 2024/12/31 16:20:29 by dohyuki2          #+#    #+#             */
+/*   Updated: 2025/04/29 10:51:11 by dohyuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
+
+class PhoneBook
+{
+	private	:
+		std::string first;
+	public	:
+		Contact();
+};
+
+#endif
